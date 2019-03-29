9985af704d7912a56ca4aedea902f9f1953ee475
Poorva Nene <39101485+poorvanene@users.noreply.github.com>
2019-03-29 14:14:24 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,365 @@
+'POORVA NENE 38426682'
+
+import project5
+import sys
+from copy import copy,deepcopy
+import pygame
+
+'module with game mechanics'
+
+class Faller:
+    def __init__(self, col:str,grid:list,one: str,two :str,three: str,state:str):
+        self.grid = grid
+        self.col = int(col)
+        self.one = one
+        self.two = two
+        self.three = three
+        self.row = 0
+        self.copy = []
+        self.changed = []
+        self.bottom = Jewel(self.three, self.col -1,-1)
+        self.middle = Jewel(self.two, self.col -1,-1)
+        self.top = Jewel(self.one, self.col -1,-1)
+        self.jewels = [self.top.stat,self.middle.stat,self.bottom.stat]
+        self.state = state.state
+
+        self.count = 0
+
+    def copygrid(self) -> None:
+        '''makes a copy of grid'''
+        copy = deepcopy(self.grid)
+        return copy
+
+    def tick(self) -> None:
+        '''function that dictates what action to take once user inputs blank line (tick)'''
+        if self.state == 'fall':
+            self.continu()
+        if self.state == 'land':
+            self.land()
+        elif self.state == 'match':
+            self.match()
+        elif self.state == 'freeze':
+            self.freeze()
+        else:
+            pass
+
+
+
+
+    def continu(self) -> None:
+        '''makes the faller continue falling'''
+        copy = self.copygrid()
+        if ((self.row == 12) and (self.grid[self.row][self.col -1] is ' ')) or ((self.row < 12) and (self.grid[self.row+1][self.col -1] is not ' ')):
+            copy[self.row][self.col - 1] = ('[' + str(self.jewels[2][2]) + ']')
+            self.bottom = Jewel(str(self.jewels[2][2]),self.col -1,self.row)
+
+
+            if (self.row - 1) >= 0:
+                copy[self.row-1][self.col - 1] = ('[' + str(self.jewels[1][2]) + ']')
+                self.middle = Jewel(str(self.jewels[1][2]),self.col -1,self.row-1)
+
+
+            if (self.row - 2) >= 0:
+                copy[self.row - 2][self.col - 1] = ('[' + str(self.jewels[0][2]) + ']')
+                self.top = Jewel(str(self.jewels[0][2]), self.col -1, self.row - 2)
+
+            self.state = 'land'
+            self.copy = copy
+            self.jewels = [self.top.stat, self.middle.stat, self.bottom.stat]
+
+
+
+
+        elif (self.grid[self.row][self.col -1] is ' ') and (self.row < len(copy)-1):
+            copy[self.row][self.col - 1] = ('[' + str(self.jewels[2][2]) + ']')
+            self.bottom = Jewel(str(self.jewels[2][2]),self.col -1,self.row)
+
+
+            if (self.row - 1) >= 0:
+                copy[self.row-1][self.col - 1] = ('[' + str(self.jewels[1][2]) + ']')
+                self.middle = Jewel(str(self.jewels[1][2]),self.col -1,self.row-1)
+
+
+            if (self.row - 2) >= 0:
+                copy[self.row - 2][self.col - 1] = ('[' + str(self.jewels[0][2]) + ']')
+                self.top = Jewel(str(self.jewels[0][2]), self.col - 1, self.row - 2)
+
+
+            self.copy = copy
+            self.row += 1
+
+            self.jewels = [self.top.stat, self.middle.stat, self.bottom.stat]
+            self.state = 'fall'
+
+
+
+
+    def land(self) -> None:
+        '''makes the faller land'''
+        for a in self.jewels:
+            old = str(self.copy[a[0]][(a[1])])
+            if a[0] >= 0:
+                self.copy[a[0]][(a[1])] = ('|' + str(old[1]) + '|')
+        self.state = 'freeze'
+
+
+    def moveright(self) -> None:
+        '''makes the faller move right'''
+        if self.state is not 'ready':
+            try:
+                r = self.jewels[2][0]
+                c = self.jewels[2][1]
+                v = int(c) + 1
+                if self.copy[r][v] is ' ':
+                    for a in self.jewels:
+                        if a[0] != -1:
+                            self.copy[a[0]][(a[1]+1)] = self.copy[a[0]][(a[1])]
+                            self.copy[a[0]][(a[1])] = ' '
+                            a[1] +=1
+                    self.col += 1
+                else:
+                    pass
+            except:
+                pass
+            else:
+                if self.freebelow() == True:
+                    if self.state == 'land' or self.state == 'freeze':
+                        self.state = 'fall'
+                        self.continu()
+                elif self.freebelow() == False:
+                    if self.state == 'fall':
+                        self.state = 'land'
+                        self.land()
+                        self.row +=1
+
+        else:
+            pass
+
+
+
+    def moveleft(self) -> None:
+        '''makes faller move left'''
+        if self.state is not 'ready':
+            try:
+                r = self.jewels[2][0]
+                c = self.jewels[2][1]
+                v = int(c) + -1
+                if self.copy[r][v] is ' ':
+                    for a in self.jewels:
+                        if a[0] != -1 and a[1]-1 >=0:
+                            self.copy[a[0]][(a[1]-1)] = self.copy[a[0]][(a[1])]
+                            self.copy[a[0]][(a[1])] = ' '
+                            a[1] = a[1]- 1
+
+
+            except:
+                pass
+            else:
+                if self.jewels[2][1] >= 0:
+                    self.col = self.jewels[2][1] + 1
+                    if self.freebelow() == True:
+                        if self.state == 'land' or self.state == 'freeze':
+                            self.state = 'fall'
+                            self.continu()
+                    elif self.freebelow() == False:
+                        if self.state == 'fall':
+                            self.state = 'land'
+                            self.land()
+                            self.row +=1
+
+        else:
+            pass
+
+
+
+    def rotate(self) -> None:
+        '''makes faller rotate'''
+        three = self.jewels[2][2]
+        two = self.jewels[1][2]
+        one = self.jewels[0][2]
+        self.jewels[0][2] = three
+        self.jewels[1][2] = one
+        self.jewels[2][2] = two
+
+        for a in self.jewels:
+            if self.state == 'fall':
+                if a[0] != -1:
+                    self.copy[a[0]][a[1]] = ('[' + str(a[2]) + ']')
+            elif self.state == 'freeze' or 'land':
+                if a[0] != -1:
+                    self.copy[a[0]][a[1]] = ('|' + str(a[2]) + '|')
+
+
+
+
+
+    def freeze(self) -> None:
+        '''makes faller freeze'''
+        c = 0
+        for a in self.jewels:
+            old = str(self.copy[a[0]][(a[1])])
+            if a[0] >= 0:
+                c+=1
+                self.copy[a[0]][(a[1])] = (str(old[1]))
+        if c < 3:
+            self.state = 'done'
+        else:
+            self.state = 'ready'
+            self.grid = self.copy
+
+
+    def match(self) -> None:
+        '''changes grid if there is horizontal or vertical matching'''
+        for i in range(len(self.copy)):
+            for j in range(len(self.copy[0])):
+                if '*' in self.copy[i][j]:
+                    self.copy[i][j] = ' '
+        self.copy = dropdown(self.copy)
+        if horizontal(self.copy) == True or vertical(self.copy) == True:
+            self.state = 'match'
+        else:
+            self.state = 'ready'
+
+
+    def freebelow(self) -> bool:
+        '''returns if a jewel has a free space below or not'''
+        r = self.jewels[2][0]
+        c = self.jewels[2][1]
+        v = int(r) + 1
+        try:
+            if self.copy[v][c] == ' ':
+                return True
+            else:
+                return False
+        except:
+            return False
+
+
+
+
+class Jewel:
+    '''creates a instance of one Jewel in the faller'''
+    def __init__(self,value,column,row):
+        self.val = value
+        self.column = column
+        self.row = row
+        self.stat = [self.row,self.column,self.val]
+
+
+
+class GameOverException(Exception):
+    '''exception raised when frozen faller does not fit'''
+    def __init__(self):
+
+        sys.exit()
+
+def horizontal(grid: list) -> bool:
+    '''checks horizontal matching'''
+    index = []
+    count = 0
+    for a in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[a][c] is not ' ' and (c <= len(grid[0]) - 3):
+                if grid[a][c] == grid[a][c+1] == grid[a][c+2]:
+                    index.append([a,c])
+                    count = 1
+                    while c+count < len(grid[0])-1:
+                        try:
+                            while grid[a][c + count] == grid[a][c]:
+                                index.append([a,c + count])
+                                count +=1
+                        except:
+                            pass
+    for a in index:
+
+        old = grid[a[0]][a[1]]
+        if '*' not in grid[a[0]][a[1]]:
+            grid[a[0]][a[1]] = ('*' + old + '*')
+            count +=1
+    if len(index) > 1:
+        return True
+    else:
+        return False
+
+def vertical(grid: list) -> bool:
+    '''checks vertical matching'''
+    index = []
+    count = 0
+    for a in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[a][c] is not ' ' and (a <= len(grid) - 3):
+                if grid[a][c] == grid[a+1][c] == grid[a+2][c]:
+                    index.append([a,c])
+                    count = 1
+                    while a+count < len(grid)-1:
+                        try:
+                            while grid[a + count][c] == grid[a][c]:
+                                index.append([a + count,c])
+                                count +=1
+                        except:
+                            pass
+    for a in index:
+        old = grid[a[0]][a[1]]
+        if '*' not in grid[a[0]][a[1]]:
+            grid[a[0]][a[1]] = ('*' + old + '*')
+            count +=1
+    if len(index) > 1:
+        return True
+    else:
+        return False
+
+
+def checker(grid: list) -> bool:
+    '''checks if board has no holes'''
+    result = True
+    for r in range(len(grid) - 1):
+        for c in range(len(grid[0])):
+            if (grid[r][c] is not ' ') and (grid[r + 1][c] == ' '):
+                result = False
+    return result
+
+
+
+def dropdown(grid) -> list:
+    '''fills holes in grid'''
+    while checker(grid) == False:
+        for r in range(len(grid)-1):
+            for c in range(len(grid[0])):
+                if (grid[r][c] is not ' ') and (grid[r+1][c] == ' '):
+                    val = grid[r][c]
+                    grid[r][c] = ' '
+                    grid[r+1][c] = val
+
+
+    return grid
+
+
+
+def startgrid():
+    '''creates starting grid'''
+    grid = [[' ' for x in range(int(6))] for y in range(int(13))]
+
+    return grid
+
+
+
+class GameState():
+    '''creates initial gamestate'''
+    def __init__(self):
+        self.state = 'fall'
+    def falling(self):
+        self.state = 'fall'
+    def landing(self):
+        self.state = 'land'
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project5.py
@@ -0,0 +1,165 @@
+'POORVA NENE 38426682'
+import game
+import pygame
+import random
+
+'module with user interface'
+
+
+class Game:
+    def __init__(self):
+        '''initializes faller and state'''
+        self.running = True
+        self.f = None
+        self.faller = None
+
+
+
+    def run(self) -> None:
+        '''main user interface'''
+        T = (127, 255, 212)
+        R = (255,64,64)
+        P = (255,20,147)
+        Y = (255,215,0)
+        G = (69,139,0)
+        B = (100,149,237)
+        L = ((191,62,255))
+
+        grid = game.startgrid()
+        g = game.GameState()
+        f = self.randomfaller()
+        self.f = f
+        faller = game.Faller(f[0], grid, f[1], f[2], f[3], g)
+        self.faller = faller
+        pygame.init()
+        self.resizesurface((750, 750))
+        clock = pygame.time.Clock()
+
+        t = 0
+        while self.running:
+            clock.tick(200)
+            if self.faller.state == 'done':
+                surface = pygame.display.get_surface()
+                surface.fill(pygame.Color(0, 0, 0))
+                pygame.font.init()
+                myfont = pygame.font.SysFont('Arial', 100, 1, 1)
+                textsurface = myfont.render('GAME OVER', True, (255, 215, 0))
+                surface.blit(textsurface, (137, 205))
+                pygame.display.flip()
+            self.handle_events()
+            if t%60 == 0:
+                if self.faller.state == 'ready':
+                    newgrid = self.faller.copy
+                    f = self.randomfaller()
+                    self.f = f
+                    faller = game.Faller(f[0], newgrid, f[1], f[2], f[3], g)
+                    self.faller = faller
+                self.faller.tick()
+                self.redraw(self.faller.copy)
+            t = t + 1
+
+        pygame.quit()
+
+    def handle_events(self) -> None:
+        '''handles events'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.key_handle(event.key)
+
+    def key_handle(self,key) -> None:
+        '''handles key pressed events'''
+        if key == pygame.K_SPACE:
+            self.faller.rotate()
+        elif key == pygame.K_RIGHT:
+            self.faller.moveright()
+        elif key == pygame.K_LEFT:
+            self.faller.moveleft()
+
+    def drawboard(self,grid) -> None:
+        '''draws board & grid'''
+        surface = pygame.display.get_surface()
+        pygame.font.init()
+        myfont = pygame.font.SysFont('Arial', 65,1,1)
+        textsurface = myfont.render('C O L U M N S', True, (255,215,0))
+        surface.blit(textsurface, (205, 50))
+
+        yval = 100
+        for a in grid:
+            yval += 43
+            xval = 200
+            for c in a:
+                xval += 43
+                self.drawjewels(c,xval,yval)
+
+    def redraw(self,grid) -> None:
+        '''redraws surface'''
+        surface = pygame.display.get_surface()
+        surface.fill(pygame.Color(0, 0, 0))
+        self.drawboard(grid)
+        pygame.display.flip()
+
+
+    def randomfaller(self) -> list:
+        '''creates a random faller'''
+        colors = ['T', 'R', 'P', 'Y', 'G', 'B', 'L']
+        color1 = random.choice(colors)
+        color2 = random.choice(colors)
+        color3 = random.choice(colors)
+        col = random.randint(1, 6)
+        faller = [col, color1, color2, color3]
+        return faller
+
+    def drawjewels(self,str,xval,yval) -> None:
+        '''draws jewels based on their state'''
+        surface = pygame.display.get_surface()
+        if '[' in str or (len(str) == 1 and str is not ' '):
+            if 'T' in str:
+                pygame.draw.rect(surface, (127, 255, 212), [xval, yval, 40, 40])
+            elif 'R' in str:
+                pygame.draw.rect(surface, (255, 64, 64), [xval, yval, 40, 40])
+            elif 'P' in str:
+                pygame.draw.rect(surface, (255, 20, 147), [xval, yval, 40, 40])
+            elif 'Y' in str:
+                pygame.draw.rect(surface, (255, 215, 0), [xval, yval, 40, 40])
+            elif 'G' in str:
+                pygame.draw.rect(surface, (69, 139, 0), [xval, yval, 40, 40])
+            elif 'B' in str:
+                pygame.draw.rect(surface, (100, 149, 237), [xval, yval, 40, 40])
+            elif 'L' in str:
+                pygame.draw.rect(surface, ((191, 62, 255)), [xval, yval, 40, 40])
+        elif '|' in str:
+            if 'T' in str:
+                pygame.draw.rect(surface, (127, 255, 212), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'R' in str:
+                pygame.draw.rect(surface, (255, 64, 64), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'P' in str:
+                pygame.draw.rect(surface, (255, 20, 147), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'Y' in str:
+                pygame.draw.rect(surface, (255, 215, 0), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'G' in str:
+                pygame.draw.rect(surface, (69, 139, 0), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'B' in str:
+                pygame.draw.rect(surface, (100, 149, 237), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+            elif 'L' in str:
+                pygame.draw.rect(surface, ((191, 62, 255)), [xval, yval, 40, 40])
+                pygame.draw.circle(surface, (0, 0, 0), [xval + 20, yval + 20], 6)
+        else:
+            pygame.draw.rect(surface, (255, 255, 255), [xval, yval, 40, 40])
+
+
+    def resizesurface(self, size: (int, int)) -> None:
+        '''makes window resizable'''
+        pygame.display.set_mode(size, pygame.RESIZABLE)
+
+
+
+if __name__ == '__main__':
+    Game().run()
